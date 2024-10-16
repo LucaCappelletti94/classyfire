3d14e7b385c1605caf80185af506eb1260d7153b
Luca <cappelletti.luca94@gmail.com>
2024-10-16 17:31:13 +0200

    Extended test suite

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,6 +1,7 @@
 """Test commands of the CLI to check if they are working as expected."""
 
 import os
+from typing import Dict, List
 import subprocess
 import compress_json
 from classyfire import ClassyFire, Compound
@@ -22,3 +23,29 @@ def test_classify_inchikey():
     )
     assert os.path.exists(output)
     assert expected == Compound.from_dict(compress_json.load(output))
+
+
+def test_classify_csv():
+    """Test classify_csv command."""
+    expected: List[Dict[str, Compound]] = list(ClassyFire().classify_csv("tests/example.csv"))
+    output = "output.json"
+    subprocess.run(
+        [
+            "classyfire",
+            "tests/example.csv",
+            "--output",
+            output,
+        ],
+        check=True,
+    )
+    assert os.path.exists(output)
+
+    loaded: List[Dict[str, Compound]] = [
+        {
+            column_name: Compound.from_dict(compound)
+            for column_name, compound in compounds.items()
+        }
+        for compounds in compress_json.load(output)
+    ]
+
+    assert expected == loaded
